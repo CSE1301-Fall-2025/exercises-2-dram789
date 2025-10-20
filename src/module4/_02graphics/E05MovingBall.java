46347ed2fc0b14b1fbcf76e0faa99e5b0b578224
dram789 <dram@wustl.edu>
2025-10-20 14:26:22 -0500

    more edits

--- src/module4/_02graphics/E05MovingBall.java
+++ src/module4/_02graphics/E05MovingBall.java
@@ -1,6 +1,7 @@
 package module4._02graphics;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 import edu.princeton.cs.introcs.StdDraw;
 
@@ -11,28 +12,66 @@ public class E05MovingBall {
 	 * understand how it works before proceeding.
 	 */
 	public static void main(String[] args) {
-		double ballX = 0.0;
-		
-		StdDraw.setPenColor(Color.RED);
+		double ballX = Math.random();
+		double ballXVelocity = Math.random() * 0.05;
+		double ballY = Math.random();
+		double ballYVelocity = Math.random() * 0.05;
+
+		double playerX = 0.5;
+		double playerY = 0.5;
+		double playerSpeed = 0.05;
+
+		int score = 0;
+
+		long lastPointGiven = System.currentTimeMillis();//long = same as an int but bigger
+
+		StdDraw.setPenColor(Color.BLUE);
 		StdDraw.setPenRadius(0.05);
 		
+		//infinite animation loop
 		while (true) {
 			
-			//Clear the canvas
+			//step 1 - clear the canvas
 			StdDraw.clear();
 			
-			//update the ball position
-			ballX = ballX + 0.005;
-			if(ballX > 1) { //too far right
-				ballX = 0; //back to left
+			//step 2 - updates posiiton of ALL moving addresses
+			ballX = ballX + ballXVelocity;
+			ballY = ballY + ballYVelocity;
+			if(ballX > 0.975 || ballX < 0) { //too far right
+				ballX =- ballXVelocity; //bounce
+			}
+			if(ballY > 0.975 || ballY < 0) { //too far right
+				ballY =- ballYVelocity; //bounce
+			}
+
+			if(StdDraw.isKeyPressed(KeyEvent.VK_W)){//need to go up
+				playerY += playerSpeed;
+			}
+
+			long now = System.currentTimeMillis();
+			if(now - lastPointGiven > 1000){//one second elapsed
+				score++;
+				lastPointGiven = now;
 			}
 			
-			//draw on canvas
-			StdDraw.point(ballX, 0.5);
+			//step 3 - draw on canvas and pause
+			StdDraw.setPenColor(Color.RED);
+			StdDraw.point(ballX, ballY);
+			StdDraw.setPenColor(Color.BLACK);
+			StdDraw.point(playerX, playerY);
+			StdDraw.text(0.5, 0.1, "Score: " + score);
 			
 			//pause to make the animation smooth
 			StdDraw.show();
 			StdDraw.pause(10);
+
+			double distance = Math.sqrt(Math.pow(playerX - ballX, 2) + Math.pow(playerY - ballY, 2));
+			
+			if(distance < 0.1){ //collision!
+				playerX = 0.5;
+				playerY = 0.5;
+				score = 0;
+			}
 			
 		}
 	}
